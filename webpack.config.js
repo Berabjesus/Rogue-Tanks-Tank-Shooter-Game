8cd4516b150e524835cbea64acb6d65dc2c5460e
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-01-02 14:48:26 +0300

    ADD webpack config

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins() {
+                return [
+                  /* eslint-disable global-require */
+                  require('autoprefixer'),
+                /* eslint-enable global-require */
+                ];
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|mp3|ogg|woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin({ 
+      root: path.resolve(__dirname, "../")
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new CopyPlugin ([
+      {
+        from: path.resolve(__dirname, 'assets', '**','*'),
+        to: path.resolve(__dirname, 'build')
+      }
+    ]),
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER':JSON.stringify(true)
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    port: 3001,
+  },
+};

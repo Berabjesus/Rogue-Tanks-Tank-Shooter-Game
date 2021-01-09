8df860c7ae79e8b6c6984f4df2da1c85dc70a638
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-01-09 16:58:38 +0300

    ADD intro voice after debugging

--- src/Scenes/IntroScene.js
+++ src/Scenes/IntroScene.js
@@ -59,7 +59,7 @@ export default class IntroScene extends Phaser.Scene {
 
     this.bgMusic = this.sys.game.globals.bgMusic
     this.bgMusic.volume = 0.1
-    const intro = this.sound.add('fire', { volume: 0.5});
+    const intro = this.sound.add('introVoice', { volume: 0.5});
     intro.on('complete', function() {
       if(!this.skipped)
         this.setPlayButtonActive()

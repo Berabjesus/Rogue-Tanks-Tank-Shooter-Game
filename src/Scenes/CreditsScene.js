1ba8f6efbbb5cbd6c74cda81153fba59d4a4af9d
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-01-10 02:59:55 +0300

    REFACTOR credits scene

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -8,29 +8,29 @@ export default class CreditsScene extends Phaser.Scene {
 
   create() {
 
-    this.creditsText = this.add.text(0, 0, 'Credits', {
+    const creditsText = this.add.text(0, 0, 'Credits', {
       fontSize: '32px',
       fill: '#fff'
     });
-    this.madeByText = this.add.text(0, 0, 'Created By: Bereket A, Beshane \nMade With: Phaser 3.5, Javascript ES6\nGithub: @Berabjesus', {
+    const madeByText = this.add.text(0, 0, 'Created By: Bereket A, Beshane \nMade With: Phaser 3.5, Javascript ES6\nGithub: @Berabjesus', {
       fontSize: '26px',
       fill: '#fff'
     });
-    this.zone = this.add.zone(this.cameras.main.width / 2, this.cameras.main.height / 2, this.cameras.main.width, this.cameras.main.height);
+    const zone = this.add.zone(this.cameras.main.width / 2, this.cameras.main.height / 2, this.cameras.main.width, this.cameras.main.height);
     Phaser.Display.Align.In.Center(
-      this.creditsText,
-      this.zone
+      creditsText,
+      zone
     );
 
     Phaser.Display.Align.In.Center(
-      this.madeByText,
-      this.zone
+      madeByText,
+      zone
     );
 
-    this.madeByText.setY(1000);
+    madeByText.setY(1000);
 
-    this.creditsTween = this.tweens.add({
-      targets: this.creditsText,
+    this.tweens.add({
+      targets: creditsText,
       y: -100,
       ease: 'Power1',
       duration: 3000,
@@ -40,14 +40,14 @@ export default class CreditsScene extends Phaser.Scene {
       }
     });
 
-    this.madeByTween = this.tweens.add({
-      targets: this.madeByText,
+    this.tweens.add({
+      targets: madeByText,
       y: -200,
       ease: 'Power1',
       duration: 15000,
       delay: 1000,
       onComplete: function () {
-        this.madeByTween.destroy;
+        this.destroy;
         this.scene.start('Menu');
       }.bind(this)
     });

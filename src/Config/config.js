308dcb4d49df2c2b53aa1534b5e4920779459297
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-01-10 23:07:55 +0300

    ADD readme file content

--- src/Config/config.js
+++ src/Config/config.js
@@ -13,8 +13,10 @@ export default {
   scale: {
     zoom: 1,
   },
-  // pixelArt: true,
-  // roundPixels: true,
+  /*
+  pixelArt: true,
+  roundPixels: true,
+  */
   parent: 'main',
   dom: {
     createContainer: true,
